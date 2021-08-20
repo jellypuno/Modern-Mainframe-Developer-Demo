7e0489784d7dd6d91c1abefc773f7001125f6686
Jessielaine Punongbayan <jessielaine.punongbayan@broadcom.com>
2021-08-20 12:53:23 +0000

    test

---
+++ test.cbl
@@ -0,0 +1,8 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
+       PROCEDURE DIVISION.
+      *                         testing tab
+           DISPLAY 'HELLO WORLD ØåæÅ!`'
+           DISPLAY 'HELLO WORLD {|}Ý~@$£££Ý'
+           DISPLAY 'HELLO WORLD {|}Ý~@$$Ý¨Ý'
+           STOP RUN.
